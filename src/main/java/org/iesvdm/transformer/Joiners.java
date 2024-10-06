a1d8eb08b2526972128fe5a6e276e95d903ebe0e
toteabe <toteabe@yahoo.es>
2024-10-06 15:49:39 +0200

    init

---
+++ src/main/java/org/iesvdm/transformer/Joiners.java
@@ -0,0 +1,17 @@
+package org.iesvdm.transformer;
+
+
+public class Joiners
+{
+    public static <T> LispList<T> zipLists(Joiner<T> joiner,LispList<T> ls1,LispList<T> ls2)
+    {
+        if(ls1.isEmpty()||ls2.isEmpty())
+            return LispList.empty();
+        else
+        {
+            T h = joiner.join(ls1.head(),ls2.head());
+            LispList<T> t = zipLists(joiner,ls1.tail(),ls2.tail());
+            return t.cons(h);
+        }
+    }
+}
---
+++ src/main/java/org/iesvdm/transformer/Transformers.java
@@ -0,0 +1,15 @@
+package org.iesvdm.transformer;
+
+import java.util.ArrayList;
+
+public class Transformers
+{
+    public static <T> ArrayList<T> applyConst(Transformer<T> tran, ArrayList<T> a)
+    {
+        ArrayList<T> b = new ArrayList<>();
+        for(T t : a)
+            b.add(tran.transform(t));
+        return b;
+    }
+}
+
